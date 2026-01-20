1ba40377846c0efce808eb822279cbd7a113d81f
Paul Woods <mr.paul.woods@gmail.com>
2026-01-19 21:34:58 -0600

    chore: bump version to 0.0.2 and update publish script accordingly

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 # make sure the version number matches the version in package.json
 docker build -t prompt-vault .
-docker tag prompt-vault paulwoods/prompt-vault:0.0.1
+docker tag prompt-vault paulwoods/prompt-vault:0.0.2
 docker tag prompt-vault paulwoods/prompt-vault:latest
-docker push paulwoods/prompt-vault:0.0.1
+docker push paulwoods/prompt-vault:0.0.2
 docker push paulwoods/prompt-vault:latest
